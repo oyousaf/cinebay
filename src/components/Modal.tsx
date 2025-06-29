9c21168af4c96b02dadc25af53bc1a53510658bf
Omar 🍉 <o_yousaf@live.co.uk>
2025-06-29 14:10:53 +0100

    optimising UI

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -129,7 +129,12 @@ export default function Modal({
 
                 <div className="flex flex-wrap gap-2 sm:gap-3 text-sm sm:text-base text-zinc-300 pt-2">
                   {!isPerson && movie.isNew && (
-                    <span className="bg-amber-400 text-black text-xs font-bold p-2 rounded shadow">
+                    <span
+                      className="bg-amber-400 text-black text-xs font-bold p-2 rounded shadow"
+                      style={{
+                        boxShadow: "0 0 6px #fbbf24, 0 0 12px #facc15",
+                      }}
+                    >
                       NEW
                     </span>
                   )}
