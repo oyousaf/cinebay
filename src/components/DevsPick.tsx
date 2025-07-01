1c4ccde8669988f1120747096ae32f33dd311ee6
Omar 🍉 <o_yousaf@live.co.uk>
2025-07-01 20:56:22 +0100

    optimising devsPick

--- src/components/DevsPick.tsx
+++ src/components/DevsPick.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import Scroll from "@/components/Scroll";
-import { DEVS_PICK_TITLES } from "@/lib/constants/devsPick";
+import { DEVS_PICK_LIST } from "@/lib/constants/devsPick";
 import { fetchDevsPick } from "@/lib/tmdb";
 import type { Movie } from "@/types/movie";
 
@@ -11,7 +11,7 @@ export default function DevsPick({
 }) {
   const [movies, setMovies] = useState<Movie[]>([]);
   useEffect(() => {
-    fetchDevsPick(DEVS_PICK_TITLES).then(setMovies);
+    fetchDevsPick().then(setMovies);
   }, []);
 
   return <Scroll title="Dev's Pick" items={movies} onSelect={onSelect} />;
