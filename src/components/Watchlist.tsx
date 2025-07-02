c1e11cba8f5109a6912b0e290253989a627f2aa5
Omar 🍉 <o_yousaf@live.co.uk>
2025-07-02 15:23:17 +0100

    optimising watchlist

--- src/components/Watchlist.tsx
+++ src/components/Watchlist.tsx
@@ -80,6 +80,7 @@ export default function Watchlist({
                     animate={{ opacity: 1, scale: 1 }}
                     exit={{ opacity: 0, scale: 0.9 }}
                     transition={{ layout: { duration: 0.3 } }}
+                    onClick={() => onSelect(movie)}
                     whileHover={{
                       scale: 1.07,
                       transition: {
