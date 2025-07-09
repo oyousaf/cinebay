c1a130572e8fe8695f1492d56c707fe74708c18d
Omar 🍉 <o_yousaf@live.co.uk>
2025-07-09 19:00:11 +0100

    squashing bugs

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-20fbb2ef'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/",
-    "revision": "0.8fpb3s6i3to"
+    "revision": "0.bq9safgu7d"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
