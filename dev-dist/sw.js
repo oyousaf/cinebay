3c95b4cd652a4bc1ba8a07184003f9be0d7f819e
Omar 🍉 <o_yousaf@live.co.uk>
2025-10-06 11:52:15 +0100

    optimising UI

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ded05b5d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/",
-    "revision": "0.ivbsphm3la"
+    "revision": "0.3grv8jo64h8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
