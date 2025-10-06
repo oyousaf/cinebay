7ccce33266b16928f6b7ee1fb19e64657e1f7b09
Omar 🍉 <o_yousaf@live.co.uk>
2025-10-06 11:06:07 +0100

    eliminating vertical scroll in tablet viewport

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ded05b5d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/",
-    "revision": "0.fcukoj7bl08"
+    "revision": "0.ivbsphm3la"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
