e79046eaec8e3cab611c81bbf0e4408144739e64
Omar 🍉 <o_yousaf@live.co.uk>
2025-10-27 10:21:51 +0000

    updating devsPick

--- src/lib/constants/devsPick.ts
+++ src/lib/constants/devsPick.ts
@@ -28,7 +28,6 @@ export const DEVS_PICK_LIST = [
   9516, // Menace II Society
   1607, // A Bronx Tale
   16136, // Juice
-  12489, // Gridlock'd
   24, // Kill Bill: Vol. 1
   393, // Kill Bill: Vol. 2
   466272, // Once Upon a Time in Hollywood
