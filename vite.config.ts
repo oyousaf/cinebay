566e8036ea7a0c4ea7ef9d816a487ff88ed2ab2e
Omar 🍉 <o_yousaf@live.co.uk>
2025-11-05 20:11:28 +0000

    optimising PWA icons

--- vite.config.ts
+++ vite.config.ts
@@ -36,12 +36,6 @@ export default defineConfig({
             src: "/icon-512.png",
             sizes: "512x512",
             type: "image/png",
-            purpose: "any maskable",
-          },
-          {
-            src: "/icon-512.png",
-            sizes: "512x512",
-            type: "image/png",
           },
           {
             src: "/icon-192.png",
