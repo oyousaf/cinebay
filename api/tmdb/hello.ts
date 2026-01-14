8e1d9450bcb04339a3e9f5f644281f9fddb0881d
Omar 🍉 <o_yousaf@live.co.uk>
2026-01-14 18:38:15 +0000

    edge runtime api removed

--- api/tmdb/hello.ts
+++
@@ -1,8 +0,0 @@
-export const config = { runtime: "edge" };
-
-export default async function handler() {
-  return new Response(JSON.stringify({ message: "Backend active!" }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
-}
--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ded05b5d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/",
-    "revision": "0.ls2pvsjfne8"
+    "revision": "0.hvfksc9sp18"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
