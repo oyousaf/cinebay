f9d29cd6235bec0529105350f38984a39292cb1d
Omar 🍉 <o_yousaf@live.co.uk>
2026-01-21 14:05:52 +0000

    removed redundant file

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-bb136d98'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/",
-    "revision": "0.aa95q0s4kb"
+    "revision": "0.lpadgv96qd8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
