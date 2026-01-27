7980b6419e1e7961b8c514ec78f753538fe1d55e
Omar 🍉 <o_yousaf@live.co.uk>
2026-01-27 12:29:24 +0000

    excluded from precache

--- vite.config.ts
+++ vite.config.ts
@@ -44,7 +44,7 @@ export default defineConfig({
         clientsClaim: true,
         navigateFallback: "/",
 
-        /* ⛔ Correct way to exclude from precache (typed) */
+        /* ⛔ Correct way to exclude from precache */
         globIgnores: [
           "**/mc.yandex.ru/**",
           "**/google-analytics.com/**",
