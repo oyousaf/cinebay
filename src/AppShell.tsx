1019c00077e98476f0a33a4960565a11acff3db2
Omar 🍉 <o_yousaf@live.co.uk>
2026-02-22 16:13:01 +0000

    updated dependencies]

--- src/AppShell.tsx
+++ src/AppShell.tsx
@@ -66,7 +66,7 @@ export default function AppShell() {
       default:
         return null;
     }
-  }, [activeTab, openContent, isModalOpen]);
+  }, [activeTab, openContent]);
 
   return (
     <Layout isModalOpen={isModalOpen}>
