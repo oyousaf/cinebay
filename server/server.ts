3888fefa2b9079f74612ed3fec1c4314d1b07aa7
Omar 🍉 <o_yousaf@live.co.uk>
2026-02-25 17:55:21 +0000

    rocket emoji added

--- server/server.ts
+++ server/server.ts
@@ -63,5 +63,5 @@ app.get("/api/tmdb/*", async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
